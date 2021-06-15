3a5a911e0cac898d853bb0b3facb0f3f20b14008
debu <devenderkumar1605@gmail.com>
2021-06-15 18:22:54 +0530

    new maven

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -44,4 +44,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  
+     @Test
+  public void greetShouldIncludeGreetinginlinux() {
+    String someone = "World, I'm in linux ";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
